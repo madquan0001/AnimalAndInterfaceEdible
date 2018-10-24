768980d802055156ff9f8d98c8648f9f94840d65
Quân <quan199877@gmail.com>
2018-10-24 16:22:50 +0700

    Animal and Interface Edible

---
+++ src/com/company/Animal/Animal.java
@@ -0,0 +1,5 @@
+package com.company.Animal;
+
+public abstract class Animal {
+    public abstract String makeSound();
+}
---
+++ src/com/company/Fruit/Fruit.java
@@ -0,0 +1,7 @@
+package com.company.Fruit;
+
+import edible.Edible;
+
+public abstract class Fruit implements Edible {
+
+}
